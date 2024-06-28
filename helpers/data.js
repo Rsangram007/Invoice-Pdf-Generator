b15ffbcd26392fafef74c9ef22cb27e0c5a5b493
Rsangram007 <sangrambeheraa007@gmail.com>
2024-06-28 12:00:05 +0530

    first commit

---
+++ helpers/data.js
@@ -0,0 +1,26 @@
+module.exports = [
+    {
+        name: "Camera",
+        description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quod ullam repudiandae provident, deleniti ratione ipsum sunt porro deserunt",
+        unit:"pack",
+        quantity: 2,
+        price: 20,
+        imgurl: "https://www.pexels.com/photo/black-fujifilm-dslr-camera-90946/"
+    },
+    {
+        name: "Phone",
+        description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quod ullam repudiandae provident, deleniti ratione ipsum sunt porro deserunt",
+        unit:"pack",
+        quantity: 4,
+        price: 80,
+        imgurl: "https://www.pexels.com/photo/assorted-iphone-lot-341523/"
+    },
+    {
+        name: "Lipstick",
+        description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quod ullam repudiandae provident, deleniti ratione ipsum sunt porro deserunt",
+        unit:"pack",
+        quantity: 3,
+        price: 60,
+        imgurl: "https://www.pexels.com/photo/photo-of-pink-cosmetics-2587370/"
+    },
+]
